8f4e5fad32bc795af5981930edbe05cec4704a58
Han <han@nimbusfly.co>
2018-01-26 13:47:48 -0500

    image src on the item, this should be it's own paperclip record but this will do for now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171225113705) do
+ActiveRecord::Schema.define(version: 20180126181726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20171225113705) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "vendor_id"
+    t.string "image_src"
     t.index ["vendor_id"], name: "index_items_on_vendor_id"
   end
 
@@ -45,15 +46,6 @@ ActiveRecord::Schema.define(version: 20171225113705) do
     t.datetime "updated_at", null: false
     t.bigint "vendor_id"
     t.index ["vendor_id"], name: "index_operational_hours_on_vendor_id"
-  end
-
-  create_table "vendors", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "description"
-    t.string "latitude"
-    t.string "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -82,6 +74,15 @@ ActiveRecord::Schema.define(version: 20171225113705) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
+  create_table "vendors", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.string "latitude"
+    t.string "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "discounts", "items"
