d9fc0f16ad65f01ecb97bff6a13442c1ab9b0a8b
H-KE <michael.kehan@gmail.com>
2018-05-20 19:15:30 -0400

    fix up cart screen total

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -10,7 +10,7 @@ class CartController < ApplicationController
         item['total'] = '%.2f' % (quantity * item['price'])
         item_array.push(item)
       end
-      vendor_hash["total"] = vendor_hash["items"].sum { |i| i['total'] }
+      vendor_hash["total"] = '%.2f' % (vendor_hash["items"].sum { |i| i['total'].to_f })
       # vendor_hash["items"] = Item.find(items.keys).as_json(methods: :image_file_src)
       populated_cart.push(vendor_hash)
     end
